51962f00f8710e93f09a45f89a9ce10539b671cf
avbravo <avbravo@avbravo-ThinkCentre-M70t-Gen-3>
2026-07-13 16:09:14 -0500

    master

--- src/main/java/io/jettra/flux/theme/ThemeData.java
+++ src/main/java/io/jettra/flux/theme/ThemeData.java
@@ -36,6 +36,26 @@ public class ThemeData {
     public String generateGlobalCss() {
         return "<style>\n" +
                "body { background-color: " + backgroundColor + "; color: " + onSurfaceColor + "; font-family: sans-serif; margin: 0; padding: 0; }\n" +
+               "/* JettraFlux Responsive Grid Layout */\n" +
+               ".espresso-dashboard {\n" +
+               "  display: grid;\n" +
+               "  grid-template-areas: 'top top' 'left center' 'left footer';\n" +
+               "  grid-template-columns: 250px 1fr;\n" +
+               "  grid-template-rows: auto 1fr auto;\n" +
+               "  min-height: 100vh;\n" +
+               "}\n" +
+               ".espresso-top { grid-area: top; width: 100%; box-sizing: border-box; }\n" +
+               ".espresso-left { grid-area: left; height: 100%; box-sizing: border-box; }\n" +
+               ".espresso-center { grid-area: center; overflow: auto; width: 100%; box-sizing: border-box; }\n" +
+               ".espresso-footer { grid-area: footer; width: 100%; box-sizing: border-box; }\n" +
+               "@media (max-width: 768px) {\n" +
+               "  .espresso-dashboard {\n" +
+               "    grid-template-areas: 'top' 'left' 'center' 'footer';\n" +
+               "    grid-template-columns: 1fr;\n" +
+               "    grid-template-rows: auto auto 1fr auto;\n" +
+               "  }\n" +
+               "  .espresso-left { height: auto; }\n" +
+               "}\n" +
                "</style>\n";
     }
 }
